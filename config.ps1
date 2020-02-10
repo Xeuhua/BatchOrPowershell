ac86004a5b70e14c8f4e0a5ea9258c1b2a92e198
Xuehua <33216835+Xeuhua@users.noreply.github.com>
2020-02-10 03:59:47 +0100

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,6 +1,14 @@
 #ps
 #find root of the .properties file
-$info=(get-Content "<filename>.properties" -totalcount 1) -split '='
+$info=(get-Content "env.properties" -totalcount 1) -split '='
 set-item -force -path "env:$($info[0])" -value $info[1]
 
-#load all the infos from the .properties file 
+#load all the infos from the .properties file in For-Loop
+infos=get-Content "env.properties"
+for ($i=0; $i -lt $infos.Length; $i++) {
+$info=((get-Content "env.properties")[$i]) -split '='
+set-item -force -path "env:$($info[0])" -value $info[1]
+}
+
+#check 
+ls env:
