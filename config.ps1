0cf4c31d06f69b33d7a54107075d4ed800e31e1d
Xuehua <33216835+Xeuhua@users.noreply.github.com>
2020-02-10 04:03:11 +0100

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -4,7 +4,7 @@ $info=(get-Content "env.properties" -totalcount 1) -split '='
 set-item -force -path "env:$($info[0])" -value $info[1]
 
 #load all the infos from the .properties file in For-Loop
-infos=get-Content "env.properties"
+$infos=get-Content "env.properties"
 for ($i=0; $i -lt $infos.Length; $i++) {
 $info=((get-Content "env.properties")[$i]) -split '='
 set-item -force -path "env:$($info[0])" -value $info[1]
